d52bed4fbe108f11f97d469f13fc2137f6c27cbc
Towkir Ahmed <towkir17@gmail.com>
2018-10-24 00:02:25 +0600

    codes improved, modal added for game result

--- game.js
+++ game.js
@@ -6,13 +6,13 @@ var playerRotation = 0, botRotation = 180, botSpin = 0;
 var playerChoosed = "", botChoosed = ""; // values will be initialized by upcoming functions;
 
 function rotatePlayerLeft() { // left rotatoin is a positive rotation angel;
-  playerRotation += 120;
-  player.style = "transform: rotate(" + playerRotation + "deg);";
+    playerRotation += 120;
+    player.style = "transform: rotate(" + playerRotation + "deg);";
 }
 
-function rotatePlayerRight(){ // right roation is negative angel;
-  playerRotation -= 120;
-  player.style = "transform: rotate(" + playerRotation + "deg);";
+function rotatePlayerRight() { // right roation is negative angel;
+    playerRotation -= 120;
+    player.style = "transform: rotate(" + playerRotation + "deg);";
 }
 
 left.addEventListener("click", rotatePlayerLeft);
@@ -24,87 +24,106 @@ botChoice = document.getElementsByClassName("botChoice"); // and bot choice;
 
 // rotating the bot will use random angel values and randomly positive or negative;
 rotationDirection = [1, -1];
+
 function rotateBot() {
-  botSpin = Math.floor(Math.random()*9) * rotationDirection[Math.floor(Math.random()*rotationDirection.length)];
-  botRotation += botSpin * 120;
-  // calculation is like [some random number within 9 * 120 * +1 or -1] ;
-  bot.style = "transform: rotate(" + botRotation + "deg);";
-  playerChoosed = this.id;
+    botSpin = Math.floor(Math.random() * 9) * rotationDirection[Math.floor(Math.random() * rotationDirection.length)];
+    botRotation += botSpin * 120;
+    // calculation is like [some random number within 9 * 120 * +1 or -1] ;
+    bot.style = "transform: rotate(" + botRotation + "deg);";
+    playerChoosed = this.id;
 }
 
-// a function for waiting a 0.8 sec and then reload the page;
-function waitAndReload() {
-  setTimeout(function(){location.reload();}, 800);
-}
+// for managing the game result;
+var modalParent = document.getElementById('modal-container');
+var modal = modalParent.getElementsByClassName('custom-modal')[0];
+
+var showModal = function (result) {
+    modalParent.style.opacity = '1';
+    modalParent.style.zIndex = '100';
+    modal.style.zIndex = '200';
+    modal.getElementsByTagName('h2')[0].innerHTML = result;
+    modal.getElementsByTagName('button')[0].addEventListener('click', function () {
+        window.location.reload(true);
+    });
+};
 
 // before we apply bot rotation on playerChoices, we need the wincheck function too;
 function winCheck() {
-  // to get the botChoosed option, spin number doesn't help here; we will check if elements are in viewport;
-  // and decide what has the bot choosen;
-  for (i=0;i<botChoice.length;i++){
-    if (botChoice[i].getBoundingClientRect().left > 0 && botChoice[i].getBoundingClientRect().top > 0) {
-      botChoosed = botChoice[i].id;
-    } else {
-      // left empty;
+    // to get the botChoosed option, spin number doesn't help here; we will check if elements are in viewport;
+    // and decide what has the bot choosen;
+    for (i = 0; i < botChoice.length; i++) {
+        if (botChoice[i].getBoundingClientRect().left > 0 && botChoice[i].getBoundingClientRect().top > 0) {
+            botChoosed = botChoice[i].id;
+        }
     }
-  }
-  // now that we know what has the bot and the player choosen, we can check for the winner:
-  if (playerChoosed == botChoosed) {
-    if (playerChoosed == "rock" && botChoosed == "rock") {
-      playerChoice[0].style = "background-image: url('images/rock_lost.svg');";
-      botChoice[0].style = "background-image: url('images/rock_lost.svg');";
-    } else if (playerChoosed == "scissor" && botChoosed == "scissor") {
-      playerChoice[1].style = "background-image: url('images/scissor_lost.svg');";
-      botChoice[1].style = "background-image: url('images/scissor_lost.svg');";
-    } else if (playerChoosed == "paper" && botChoosed == "paper") {
-      playerChoice[2].style = "background-image: url('images/paper_lost.svg');";
-      botChoice[2].style = "background-image: url('images/paper_lost.svg');";
+    // now that we know what has the bot and the player choosen, we can check for the winner:
+    if (playerChoosed == botChoosed) {
+        if (playerChoosed == "rock" && botChoosed == "rock") {
+            playerChoice[0].style = "background-image: url('images/rock_lost.svg');";
+            botChoice[0].style = "background-image: url('images/rock_lost.svg');";
+        } else if (playerChoosed == "scissor" && botChoosed == "scissor") {
+            playerChoice[1].style = "background-image: url('images/scissor_lost.svg');";
+            botChoice[1].style = "background-image: url('images/scissor_lost.svg');";
+        } else if (playerChoosed == "paper" && botChoosed == "paper") {
+            playerChoice[2].style = "background-image: url('images/paper_lost.svg');";
+            botChoice[2].style = "background-image: url('images/paper_lost.svg');";
+        }
+
+        setTimeout(function () {
+            // alert("Its a tie !!");
+            showModal("It's a Tie");
+        }, 500); // waiting for the transition (image changing);
+    } else if (playerChoosed == "rock" && botChoosed == "scissor") {
+        playerChoice[0].style = "background-image: url('images/rock_won.svg');";
+        botChoice[1].style = "background-image: url('images/scissor_lost.svg');";
+        setTimeout(function () {
+            // alert("You Win !!");
+            showModal("You Win");
+        }, 500);
+    } else if (playerChoosed == "scissor" && botChoosed == "paper") {
+        playerChoice[1].style = "background-image: url('images/scissor_won.svg');";
+        botChoice[2].style = "background-image: url('images/paper_lost.svg');";
+        setTimeout(function () {
+            showModal("You Win");
+        }, 500);
+    } else if (playerChoosed == "paper" && botChoosed == "rock") {
+        playerChoice[2].style = "background-image: url('images/paper_won.svg');";
+        botChoice[0].style = "background-image: url('images/rock_lost.svg');";
+        setTimeout(function () {
+            showModal("You Win");
+        }, 500);
     } else {
-      // left empty;
+        if (playerChoosed == "rock") {
+            playerChoice[0].style = "background-image: url('images/rock_lost.svg');";
+            botChoice[2].style = "background-image: url('images/paper_won.svg');";
+            setTimeout(function () {
+                showModal("You Lose !!");
+            }, 500);
+        } else if (playerChoosed == "scissor") {
+            playerChoice[1].style = "background-image: url('images/scissor_lost.svg');";
+            botChoice[0].style = "background-image: url('images/rock_won.svg');";
+            setTimeout(function () {
+                showModal("You Lose !!");
+            }, 500);
+        } else {
+            playerChoice[2].style = "background-image: url('images/paper_lost.svg');";
+            botChoice[1].style = "background-image: url('images/scissor_won.svg');";
+            setTimeout(function () {
+                showModal("You Lose !!");
+            }, 500);
+        }
     }
-    setTimeout(function(){alert("Its a tie !!");}, 500); // waiting for the transition (image changing);
-    waitAndReload();
-  } else if (playerChoosed == "rock" && botChoosed == "scissor") {
-      playerChoice[0].style = "background-image: url('images/rock_won.svg');";
-      botChoice[1].style = "background-image: url('images/scissor_lost.svg');";
-      setTimeout(function(){alert("You Win !!");}, 500); // same as said earlier;
-      waitAndReload();
-  } else if (playerChoosed == "scissor" && botChoosed == "paper") {
-      playerChoice[1].style = "background-image: url('images/scissor_won.svg');";
-      botChoice[2].style = "background-image: url('images/paper_lost.svg');";
-      setTimeout(function(){alert("You Win !!");}, 500);
-      waitAndReload();
-  } else if (playerChoosed == "paper" && botChoosed == "rock") {
-      playerChoice[2].style = "background-image: url('images/paper_won.svg');";
-      botChoice[0].style = "background-image: url('images/rock_lost.svg');";
-      setTimeout(function(){alert("You Win !!");}, 500);
-      waitAndReload();
-  } else {
-    if (playerChoosed == "rock") {
-      playerChoice[0].style = "background-image: url('images/rock_lost.svg');";
-      botChoice[2].style = "background-image: url('images/paper_won.svg');";
-      setTimeout(function(){alert("You Lose !!");}, 500);
-      waitAndReload();
-    } else if (playerChoosed == "scissor") {
-      playerChoice[1].style = "background-image: url('images/scissor_lost.svg');";
-      botChoice[0].style = "background-image: url('images/rock_won.svg');";
-      setTimeout(function(){alert("You Lose !!");}, 500);
-      waitAndReload();
-    } else {
-      playerChoice[2].style = "background-image: url('images/paper_lost.svg');";
-      botChoice[1].style = "background-image: url('images/scissor_won.svg');";
-      setTimeout(function(){alert("You Lose !!");}, 500);
-      waitAndReload();
-    }
-  }
 }
 
-function waitBeforeWinCheck() {setTimeout(winCheck, 1600);} // because the transition of the bot and player
-  // takes 1.5 second, we should wait till then, after the transition, we calculate their positions to get the correct value;
-  // infact, we start at 1.6 second which is 1600ms ;
+function waitBeforeWinCheck() {
+    setTimeout(winCheck, 1600);
+} // because the transition of the bot and player
+// takes 1.5 second, we should wait till then, after the transition, we calculate their positions to get the correct value;
+// infact, we start at 1.6 second which is 1600ms ;
 
 //applying botRotation on playerChoice:
-for (i=0; i<playerChoice.length; i++) {
-  playerChoice[i].addEventListener("click", rotateBot);
-  playerChoice[i].addEventListener("click", waitBeforeWinCheck);
+for (i = 0; i < playerChoice.length; i++) {
+    playerChoice[i].addEventListener("click", rotateBot);
+    playerChoice[i].addEventListener("click", waitBeforeWinCheck);
 }
+
